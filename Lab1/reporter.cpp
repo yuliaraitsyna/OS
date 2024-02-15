da00aea174a14057856ea716193d7a1be0ce04e4
Yulia Raitsyna <137895986+yuliaraitsyna@users.noreply.github.com>
2024-02-15 21:34:34 +0300

    Update reporter.cpp

--- Lab1/reporter.cpp
+++ Lab1/reporter.cpp
@@ -5,20 +5,20 @@ int main(int argc, char *argv[])
 {
     std::ifstream fin(argv[1], std::ios::binary);
     std::ofstream fout(argv[2]);
-    double salary_per_hour = atof(argv[3]);
+    double hourSalary = atof(argv[3]);
 
-    int records_num;
-    fin.read((char *)&records_num, sizeof(int));
-    employee *records = new employee[records_num];
+    int recordsNum;
+    fin.read((char *)&recordsNum, sizeof(int));
+    employee *records = new employee[recordsNum];
 
-    for (int i = 0; i < records_num; i++)
+    for (int i = 0; i < recordsNum; i++)
     {
         fin.read((char *)&records[i], sizeof(employee));
     }
 
-    for (int i = 0; i < records_num; i++)
+    for (int i = 0; i < recordsNum; i++)
     {
-        fout << "Number: " << records[i].num << " Name: " << records[i].name << " Hours: " << records[i].hours << " Total salary: " << records[i].hours * salary_per_hour << std::endl;
+        fout << "Number: " << records[i].num << " Name: " << records[i].name << " Hours: " << records[i].hours << " Total salary: " << records[i].hours * hourSalary << std::endl;
     }
     fin.close();
     fout.close();
